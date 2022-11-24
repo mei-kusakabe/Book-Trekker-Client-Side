01d48e56b62d2470f883e92fb13a2e32eb18d52a
Jafrin <jafrin3347@gmail.com>
2022-11-25 03:10:21 +0600

    404-route

--- src/Pages/FourZeroFour/FourZeroFour.js
+++ src/Pages/FourZeroFour/FourZeroFour.js
@@ -6,9 +6,9 @@ const FourZeroFour = () => {
 
             <div className="d-flex justify-content-center align-items-center h-100">
                 <div className='mt-5' >
-                    <h4 className="mb-3 fst-italic text-center fs-1 px-5 text-white"><br></br>Ooops! 404 Route Not Found.</h4>
+                    <h4 className="mb-3 fst-italic text-center fs-1 px-5 text-white"><br></br>Couldn't launch :'0 <br></br> 404 Route Not Found.</h4>
                     <a className="mb-3 btn btn-outline-light btn-lg fw-bold" href="/" role="button"
-                    >Go back to Home Page</a>
+                    >Let's take you back to Home Page</a>
 
                 </div>
             </div>
