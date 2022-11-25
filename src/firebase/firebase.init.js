93b5184b82118223c5bac5daa4d62a67b68ff899
Jafrin <jafrin3347@gmail.com>
2022-11-26 01:59:55 +0600

    env-local-setup

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -5,12 +5,20 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyA_XrU3gCEV6X2x9hJ1VUFXhrt_xwB9lxw",
-    authDomain: "book-trekker.firebaseapp.com",
-    projectId: "book-trekker",
-    storageBucket: "book-trekker.appspot.com",
-    messagingSenderId: "731211648929",
-    appId: "1:731211648929:web:34e5d4754d5b9a7b6dc5e0"
+
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+
+    // apiKey: "AIzaSyA_XrU3gCEV6X2x9hJ1VUFXhrt_xwB9lxw",
+    // authDomain: "book-trekker.firebaseapp.com",
+    // projectId: "book-trekker",
+    // storageBucket: "book-trekker.appspot.com",
+    // messagingSenderId: "731211648929",
+    // appId: "1:731211648929:web:34e5d4754d5b9a7b6dc5e0"
 };
 
 // Initialize Firebase
